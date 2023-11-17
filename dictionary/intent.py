8f9497401904ffeaf66d6b64d4ff0057ab76f169
LucaSarhan <99192966+LucaSarhan@users.noreply.github.com>
2023-11-17 16:15:45 -0300

    prova prática feita

--- dictionary/intent.py
+++ dictionary/intent.py
@@ -1,1 +1,5 @@
-intent_dict = {r"\b(?:[Qq]uero|[Cc]omo|[Mm]etodo|[Mm]étodo|[Pp]reciso)\s?(?:posso|atualizar|mudar|de)\s?(?:pagamento|atualizar|[aà]|minha)\s?(meu|forma|desatualizado|informa[cç][oõ]es)\s?(?:cart[aã]o|de)?\s?(?:pagamento|de)?\s?(?:cr[eé]dito|,)?\s?(?:o)?\s?(?:que)?\s?(?:fazer)?(\d*)", }
+intent_dict = {
+    r"\b(?:[Qq]uero|[Cc]omo|[Mm]etodo|[Mm]étodo|[Pp]reciso)\s?(?:posso|atualizar|mudar|de)\s?(?:pagamento|atualizar|[aà]|minha)\s?(meu|forma|desatualizado|informa[cç][oõ]es)\s?(?:cart[aã]o|de)?\s?(?:pagamento|de)?\s?(?:cr[eé]dito|,)?\s?(?:o)?\s?(?:que)?\s?(?:fazer)?(\d*)", "go_to1" }
+
+intent_dict2 = {
+    r"\b(?:[Qq]uero|[Cc]omo|[Oo]nde|[Ss]tatus)\s?(?:vejo|fa[cç]o|saber|de)\s?(?:o|para|onde|entrega)\s?(status|rastrear|est[áa]|,)\s?(?:do|meu|como)?\s?(?:meu|pedido|consultar)\s?(?:pedido)?\s?", "go_to2" }
